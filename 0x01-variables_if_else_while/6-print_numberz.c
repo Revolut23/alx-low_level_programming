fd59f54f3d55bbeb2186494f84e0b9cd09566b90
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-15 00:34:24 +0200

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - First point
+ * program which prints all single numbers base 10
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n == 0; n < 9;  n++)
+	{
+		putchar(n);
+	}
+	putchar("\n");
+	return (0);
+}
