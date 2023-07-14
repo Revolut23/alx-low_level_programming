4a999854d5c503a013eadac0d9a98a1b8ce6a3de
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-15 00:20:55 +0200

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - First point
+ * program which prints all single number on the base 10
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
