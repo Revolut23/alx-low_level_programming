ddc1929d71a09746ededf1b44fe9a7370a6bfdd0
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-14 23:54:38 +0200

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - First point
+ * Description: A C program prints alphabet in lower and uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
