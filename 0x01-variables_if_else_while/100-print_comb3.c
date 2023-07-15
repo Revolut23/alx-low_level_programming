0a369463f8eb013abe2eeae98d310a8903d1e084
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-15 02:19:53 +0200

    ADV1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,18 @@ int main (void)
 {
 	int i, j;
 
-	for (i = 0; i<10; i++)
-	{
-		for (j = 0; j<10; j++)
+	for (i = 0; i < 8; i++){
+		for (j = i + 1; j < 9; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-		if (i < 8 || j < 9)
+		 putchar (i + '0');
+		 putchar (j + '0');
+		if (i < 8)
 		{
-			putchar(',');
-			putchar(' ');
+		 putchar (',');
+	         putchar (' ');
 		}
-		
-		
 		}
 	}
-		putchar('\n');
-		return (0) ;
+	putchar('\n');
+	return (0) ;
 }
