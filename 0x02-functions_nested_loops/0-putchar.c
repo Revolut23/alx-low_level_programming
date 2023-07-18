8168359953d21f6cab1a9bc3a8a99379c596d08a
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-18 23:22:42 +0200

    First task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - First point
+ * description : A c program which prints _putchar
+ * Reurn: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
