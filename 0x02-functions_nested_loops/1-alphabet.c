be7332a315375c7e8c83945529ac41fa5d8e6eca
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-19 01:54:51 +0200

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase followed by a new line
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar('c');
+	putchar('\n');
+}
