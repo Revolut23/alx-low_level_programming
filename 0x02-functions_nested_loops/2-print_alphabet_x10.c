4703f6166fced0ebe691cbbf6b736bb1cb1395f1
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-19 02:13:11 +0200

    3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets in lower case 10 times
+ * followed by new line
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
