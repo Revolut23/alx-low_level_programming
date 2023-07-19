890fc445cb06b2c3ba2c7888b8e7b7deae382633
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-19 04:38:08 +0200

    5

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,7 +28,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar((r / 10) + '0');
-				_putchar((r% 10) + '0');
+				_putchar((r % 10) + '0');
 			}
 		}
 		_putchar('\n');
