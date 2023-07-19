bae4e471be3f1be75006d2b1aa5a9c2d823e7662
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-19 04:58:20 +0200

    yui

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * print_to_98 : PRINT ALL NUMBER TO IMPUT ç_
+ * @n: Starting number to count
+ */
+void print_to_98(int n);
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
