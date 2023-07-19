d0845a7124fb2d6a6c8fb9fea231cfdb12abc716
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-19 04:43:09 +0200

    yololo

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,13 @@
 
 /**
  * add - adds two integers and returns the result
- * @a: int to be added to b
- * @b: int to be added to a
- * Return: sum of a and b
+ * @n: int to be added to b
+ * @m: int to be added to a
+ * Return: sum of n and m
  */
 int add(int n, int m)
 {
 	int s;
-	
 	s = n + m;
 	return (s);
 }
