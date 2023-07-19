70b8a692d1f32047b241a42a949f5b63cff1f99b
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-19 04:35:47 +0200

    yu

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,29 +1,35 @@
-#include "main.h"
+	#include "main.h"
 
 /**
  * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
-	int i, j, result;
+	int i, j, r;
 
-	for (i = 0; i <= 9; ++i)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; ++j)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			
-			result = i * j;
-
-			if (result < 9)
+			k = j * i;
+			if (j == 0)
 			{
-				_putchar(' ');
+				_putchar(r + '0');
 			}
-			else
+
+			if (r < 10 && j != 0)
 			{
-				_putchar((result / 10) + '0');
-                        _putchar((result % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r + '0');
+			} else if (r >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((r / 10) + '0');
+				_putchar((r% 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
