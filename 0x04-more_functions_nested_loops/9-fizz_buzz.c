8d56df45618c79c03d037ed45234746a6c45542f
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-21 02:15:52 +0200

    yuty

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
-#include<stdio.h>
-#include"main.h"
+#include "main.h"
 
 /**
  * main - entry point
@@ -14,7 +13,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz");
@@ -23,10 +22,10 @@ int main(void)
 		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%i" ,i);
+			printf("%i", i);
 		if (i < 100)
 			printf(" ");
 	}
 	printf("\n");
-         return (0);	
+	return (0);
 }
