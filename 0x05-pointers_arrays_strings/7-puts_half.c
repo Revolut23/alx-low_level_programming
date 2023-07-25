844efa4ea4f35f3bcbce88f8f051fa722c1837ae
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-25 03:12:06 +0200

    yutry

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,19 +10,19 @@
 
 void puts_half(char *str)
 {
-	int length, n;
+int length, n;
 
-	for (length = 0; str[length] != '\0'; ++length)
-		;
-
-	if (length % 2 == 0)
-	{
-		for (n = length / 2; str[n] != '\0'; ++n)
-			_putchar(str[n]);
-	} else
-	{
-		for (n = ((length - 1) / 2) + 1; str[n] != '\0'; ++n)
-			_putchar(str[n]);
-	}
-	_putchar('\n');
+for (length = 0; str[length] != '\0'; ++length)
+;
+if (length % 2 == 0)
+{
+for (n = length / 2; str[n] != '\0'; ++n)
+_putchar(str[n]);
+}
+else
+{
+for (n = ((length - 1) / 2) + 1; str[n] != '\0'; ++n)
+_putchar(str[n]);
+}
+_putchar('\n');
 }
