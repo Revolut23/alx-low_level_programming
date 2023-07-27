d76ed822018f67517b1d5bcd24c672d96dc58a51
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-28 01:21:49 +0200

    usayurun

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,12 +12,13 @@ char *rot13(char *s)
 int i = 0, j;
 char *rot13 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char *ROT13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-    
-while(s[i] != '\0')
+
+while (s[i] != '\0')
 {
 for (j = 0; j < 52; j++)
 {
-if (s[i] == rot13[j]){
+if (s[i] == rot13[j])
+{
 s[i] = ROT13[j];
 break;
 }
