d01d0e2dc94264c32fad13808a1ac3834a12feac
Revolut23 <mbenjelloun2023@gmail.com>
2023-07-28 01:28:45 +0200

    all the way to advanced

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('_');
+n = -n;
+}
+if (n / 10)
+{
+print_number(n / 10);
+}
+_putchar((n % 10) + '0');
+}
