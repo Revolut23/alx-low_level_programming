70a16074f732edee38669abb6b2eeb48eb52974d
Revolut23 <mbenjelloun2023@gmail.com>
2023-08-01 01:01:10 +0200

    Useful technique

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: the longer string to search
+ * @needle: the first occurrence of the substring
+ *
+ * Return: a pointer beg of substring or @Null if it not foound.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i + j] != needle[j])
+break;
+}
+if (!needle[j])
+return (&haystack[i]);
+}
+return (NULL);
+}
