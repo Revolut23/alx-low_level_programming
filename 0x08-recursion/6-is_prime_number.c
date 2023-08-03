afd5e165f6d78ed9c81991c2019fbfed8c249d4a
Revolut23 <mbenjelloun2023@gmail.com>
2023-08-03 02:08:24 +0200

    midnight tasks

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,20 @@
 #include "main.h"
-
+/**
+ * verify_prime : check if the number is prime
+ * @n: int
+ * @m: int
+ * Return : number
+ * 
+*/
+int verify_prime(int n ,  int m)
+{
+if (m < 2 || m % n ==0)
+return (0);
+else if (n > m / 2)
+return (1);
+else
+return (verify_prime(n + 1, m));
+}
 /**
  * is_prime_number - checks to see if number is prime
  * @n:input number
@@ -7,16 +22,11 @@
  */
 int is_prime_number(int n)
 {
-int i;
-
-if (n <= 1)
-return (0);
-for (i = 2; i * i <= n; i++)
 {
-if (n % i == 0)
+if (n == 2)
 {
-return (0);
-}
-}
 return (1);
+}
+}
+return (verify_prime(2, n));
 }
