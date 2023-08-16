d544b6505278e1165617afcd25716dd6b9d5aa25
Revolut23 <mbenjelloun2023@gmail.com>
2023-08-16 23:49:24 +0200

    eh

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,15 @@
 #include "function_pointers.h"
 
 /**
-*print_name - prints a name
-*@name:name to print
-*@f: function which print name
-*/
-
+ * print_name - a function that prints a name.
+ * @name: the name given
+ * @f: function of name
+ *
+ * Rteurn: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL && f != NULL)
 
-if (name == NULL || f == NULL)
-return;
-
-f(name);
-
+		f(name);
 }
-
